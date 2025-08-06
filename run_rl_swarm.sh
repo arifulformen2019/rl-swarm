7414d3f7ece4c363e665c0a05c99952cdd7af8ce
arifulformen2019 <107299670+arifulformen2019@users.noreply.github.com>
2025-08-06 13:22:47 +0700

    Update run_rl_swarm.sh

--- run_rl_swarm.sh
+++ run_rl_swarm.sh
@@ -394,7 +394,7 @@ install_python_deps() {
         log_info "Installing $package..."
         pip install "$package"
     done
-    
+    pip install colorama
     log_info "Python dependencies installed successfully"
 }
 
